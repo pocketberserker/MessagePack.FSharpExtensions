f94270187f22f235b9ae5e30cbbba564f0c18dbf
pocketberserker <pocketberserker@gmail.com>
2017-03-17 04:54:56 +0900

    add benchmark

---
+++ sandbox/Benchmark/Program.fs
@@ -0,0 +1,87 @@
+﻿module Program
+
+open System.Collections.Immutable
+open MessagePack
+open MessagePack.Resolvers
+open MessagePack.ImmutableCollection
+open MessagePack.FSharp
+open Benchmark
+
+[<MessagePackObject>]
+type UnionSample =
+  | Foo of XYZ : int
+  | Bar of OPQ : string list
+
+let benchmark<'T> name (target: 'T) =
+
+  let iteration = 10000
+
+  let mutable data: byte [] = null
+
+  printfn "%s serialization test" typeof<'T>.Name
+  printfn ""
+
+  printf "Serialize:: "
+
+  using (new Measure(name)) (fun _ ->
+    for i in [|1..iteration|] do
+      data <- MessagePackSerializer.Serialize(target)
+  )
+
+  printf "Deserialize:: "
+
+  using (new Measure(name)) (fun _ ->
+    for i in [|1..iteration|] do
+      MessagePackSerializer.Deserialize<'T>(data)
+      |> ignore
+  )
+
+  printfn ""
+
+[<EntryPoint>]
+let main _ =
+
+  CompositeResolver.RegisterAndSetAsDefault(
+    ImmutableCollectionResolver.Instance,
+    FSharpResolver.Instance,
+    StandardResolver.Instance
+  )
+
+  [|1..10000|]
+  |> benchmark "MessagePack-CSharp"
+
+  ResizeArray([|1..10000|])
+  |> benchmark "MessagePack-CSharp"
+
+  FooClass(XYZ = 99999) :> IUnionSample
+  |> benchmark "MessagePack-CSharp"
+
+  ImmutableList<int>.Empty.AddRange([|1..10000|])
+  |> benchmark "MessagePack.ImmutableCollection"
+
+  let xs = ImmutableHashSet<int>.Empty
+  for i in [|1..10000|] do xs.Add(i) |> ignore
+  xs
+  |> benchmark "MessagePack.ImmutableCollection"
+
+  let xs = ImmutableDictionary<int, int>.Empty
+  for i in [|1..10000|] do xs.Add(i, i) |> ignore
+  xs
+  |> benchmark "MessagePack.ImmutableCollection"
+
+  [1..10000]
+  |> benchmark "MessagePack.FSharpExtensions"
+
+  [|1..10000|]
+  |> Set.ofArray
+  |> benchmark "MessagePack.FSharpExtensions"
+
+  [|1..10000|]
+  |> Array.map (fun x -> (x, x))
+  |> Map.ofArray
+  |> benchmark "MessagePack.FSharpExtensions"
+
+  Foo 99999
+  |> benchmark "MessagePack.FSharpExtensions"
+
+  0
---
+++ sandbox/README.md
@@ -0,0 +1,54 @@
+# Benchmark
+
+```
+> dotnet run -c Release -p .\Benchmark\Benchmark.fsproj
+Int32[] serialization test
+
+Serialize::   MessagePack-CSharp   568.2237 ms
+Deserialize::   MessagePack-CSharp   631.8417 ms
+
+List`1 serialization test
+
+Serialize::   MessagePack-CSharp   1056.3946 ms
+Deserialize::   MessagePack-CSharp   1088.9702 ms
+
+IUnionSample serialization test
+
+Serialize::   MessagePack-CSharp   84.7853 ms
+Deserialize::   MessagePack-CSharp   2.7591 ms
+
+ImmutableList`1 serialization test
+
+Serialize:: MessagePack.ImmutableCollection   6944.3367 ms
+Deserialize:: MessagePack.ImmutableCollection   34904.1267 ms
+
+ImmutableHashSet`1 serialization test
+
+Serialize:: MessagePack.ImmutableCollection   18.5179 ms
+Deserialize:: MessagePack.ImmutableCollection   4.2072 ms
+
+ImmutableDictionary`2 serialization test
+
+Serialize:: MessagePack.ImmutableCollection   12.2713 ms
+Deserialize:: MessagePack.ImmutableCollection   3.5704 ms
+
+FSharpList`1 serialization test
+
+Serialize:: MessagePack.FSharpExtensions   2179.4189 ms
+Deserialize:: MessagePack.FSharpExtensions   2329.7652 ms
+
+FSharpSet`1 serialization test
+
+Serialize:: MessagePack.FSharpExtensions   6019.5603 ms
+Deserialize:: MessagePack.FSharpExtensions   93316.3144 ms
+
+FSharpMap`2 serialization test
+
+Serialize:: MessagePack.FSharpExtensions   7594.696 ms
+Deserialize:: MessagePack.FSharpExtensions   103664.7891 ms
+
+UnionSample serialization test
+
+Serialize:: MessagePack.FSharpExtensions   56.2192 ms
+Deserialize:: MessagePack.FSharpExtensions   1.9478 ms
+```
